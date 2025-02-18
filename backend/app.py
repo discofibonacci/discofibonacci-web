74742e9360052ea388307f3acf922c131599628c
discofibonacci <zjacobs721@gmail.com>
2025-02-18 05:34:12 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS
+
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+
 
 @app.route("/")
 def home():
